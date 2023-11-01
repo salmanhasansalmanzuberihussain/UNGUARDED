1b70c5c373847a87bf42c914591f17d3fbef0776
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-01 17:05:21 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import SlideButton from 'rn-slide-button';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-const UNGUARDED = require('./assets/unguarded.png');
+import HomeScreen from './screens/HomeScreen';
+import SecondScreen from './screens/SecondScreen';
+import ThirdScreen from './screens/ThirdScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={UNGUARDED} style={{ width: 500, height: 500 }} />
-      <SlideButton title="Slide To Unlock" />;
-      <Button
-        title="UNGUARDED"
-        onPress={() => alert('Button pressed')}
-        color="#f3ad35"
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="UNGUARDED"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Second"
+          component={SecondScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Third"
+          component={ThirdScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
