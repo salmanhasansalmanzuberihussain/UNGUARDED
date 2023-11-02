366b69b75a0a15559383e768ba8b7d5e8b275130
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-02 08:06:45 -0400

    Add files via upload

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View, Image } from 'react-native';
+import { StyleSheet, Button, View, Image, Text } from 'react-native';
 const UNGUARDED = require('../assets/unguarded.png');
+import { RNSlidingButton, SlideDirection } from 'rn-sliding-button';
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Image source={UNGUARDED} style={{ width: 500, height: 500 }} />
-      <Button
-        title="UNGUARDED"
-        color="#f3ad35"
-        onPress={() => navigation.navigate('Second', { language: 'french' })}
-      />
+      <RNSlidingButton
+        style={{
+          width: 370,
+          backgroundColor: 'white',
+        }}
+        height={30}
+        onSlidingSuccess={() =>
+          navigation.navigate('Second', { language: 'french' })
+        }
+        slideDirection={SlideDirection.RIGHT}
+      >
+        <View>
+          <Image source={UNGUARDED} style={{ width: 50, height: 50 }} />
+        </View>
+      </RNSlidingButton>
       <StatusBar style="auto" />
     </View>
   );
