017ad06fb6446ba138886e2e6dc23b5a48c63069
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-02 20:53:37 -0400

    Add files via upload

--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -3,16 +3,17 @@ import { View, StyleSheet, Button, Image } from 'react-native';
 const UNGUARDED = require('../assets/unguarded.png');
 import QRCode from 'react-native-qrcode-svg';
 import { RNSlidingButton, SlideDirection } from 'rn-sliding-button';
+import { Linking } from 'react-native';
 
 export default function SecondScreen({ navigation, route }) {
   let logoFromFile = require('../assets/unguarded.png');
   return (
     <View style={styles.container}>
-      <QRCode value="https://qrco.de/beRqJ7" logo={logoFromFile} size={290} />
+      <QRCode value="https://qrco.de/beRqJ7" logo={logoFromFile} size={300} />
       <Button
         color="orange"
-        title="UNLOCK VIDEO"
-        onPress={() => navigation.push('Third')}
+        title="UNLOCK STORE"
+        onPress={() => Linking.openURL('http://www.unguarded.com')}
       />
       <Button
         color="orange"
